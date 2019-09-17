f8b9c448adf441f9ad8da6eb515f0265c7648ddd
Alexander <alexanderlivshits@gmail.com>
2019-09-17 18:48:25 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularBasics';
+  posts: Post[] = [
+    {title: 'title 1', text: 'text 1', id: 1},
+    {title: 'title 2', text: 'text 2', id: 2},
+    {title: 'title 3', text: 'text 3', id: 3},
+  ]
+
+  constructor() {
+  }
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+    console.log('App comp', post);
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
